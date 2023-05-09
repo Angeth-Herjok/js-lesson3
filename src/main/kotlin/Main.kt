e119086fdb3be6ceb4477fd51b47f546519dc379
Angeth-Herjok <angethherjok@gmail.com>
2023-05-10 00:50:51 +0300

    assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
+import java.util.Arrays
+
 fun main() {
+    numbersArray()
     val people=
         listOf(
             Person("John",30,168.8,50.2),
@@ -6,14 +9,34 @@ fun main() {
             Person("Anjela",23,154.2, 54.2)
         )
     val sortedByAgeDescending = people.sortedByDescending { it.age }
-    println(studentNames("John","James","Joy"))
+    println(sortedByAgeDescending)
 
+    var s=studentNames("John","James","Joy")
+    println(Arrays.toString(s))
+
+    val car=
+        listOf(
+            Car("KDG",2345.50),
+            Car("KDG",2345.50),
+            Car("KDG",2345.50),
+        )
 }
 //1. Create one function that is given the below array:
 //var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
 //(i) prints out the sum of the second and fifth elements (1 point)
+fun numbersArray() {
+    var numbers = arrayOf(32, 17, 4, 213, 78, 43, 90, 31, 3, 73, 11, 158, 62)
+    var sumNumbers=numbers[1]+numbers[4]
+    println(sumNumbers)
+
 //(ii) prints out the index of 158 (1 point)
+    println(numbers.indexOf(158))
+
 //(iii) prints out the elements in ascending order. (1 point)
+    var sortedArray = numbers.sorted()
+    println(sortedArray)
+}
+
 
 //2. Given a list of Person objects, each with the attributes,
 //name, age, height, and weight. Sort the list in order of descending age (2 points)
@@ -30,3 +53,4 @@ fun studentNames(name1:String,name2:String,name3: String):Array<String>{
 
 //4. Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of all the vehicles in the list.
+data class Car(var registration:String,var mileage:Double)
